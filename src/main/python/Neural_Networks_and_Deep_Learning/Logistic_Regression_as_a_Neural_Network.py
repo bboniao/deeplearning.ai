e13939bfbdc45752e0750f679095fdd93350a6b6
guhb <guhb@kaolafm.com>
2018-02-19 13:55:32 +0800

    init

--- src/main/python/Neural_Networks_and_Deep_Learning/Logistic_Regression_as_a_Neural_Network.py
+++ src/main/python/Neural_Networks_and_Deep_Learning/Logistic_Regression_as_a_Neural_Network.py
@@ -6,6 +6,8 @@ from PIL import Image
 from scipy import ndimage
 from Neural_Networks_and_Deep_Learning.lr_utils import load_dataset
 from Neural_Networks_and_Deep_Learning.Python_Basics_with_Numpy import  sigmoid
+import os
+
 #plt.show()
 
 
@@ -32,7 +34,7 @@ def propagate(w, b, X, Y):
     return grads, cost
 
 def optimize(w, b, X, Y, iterate_num, learning_rate, print_cost=False):
-    costs=[]
+    costs = []
     for i in range(iterate_num):
         grads, cost = propagate(w, b, X, Y)
         dw = grads["dw"]
@@ -44,10 +46,60 @@ def optimize(w, b, X, Y, iterate_num, learning_rate, print_cost=False):
         if print_cost and i % 100 == 0:
             print("Cost after iteration %i: %f" % (i, cost))
 
-        params = {"w": w,
-                  "b": b}
+    params = {"w": w,
+              "b": b}
 
-        grads = {"dw": dw,
-                 "db": db}
+    grads = {"dw": dw,
+             "db": db}
 
-        return params, grads, costs
+    return params, grads, costs
+
+def predict(w, b, X):
+    m = X.shape[1]
+    Y_prediction = np.zeros((1, m))
+    w = w.reshape(X.shape[0], 1)
+    A = sigmoid(np.dot(w.T, X) + b)
+    for i in range(A.shape[1]):
+        Y_prediction[0, i] = 1 if A[0, i] > 0.5 else 0
+    return Y_prediction
+
+
+def model(num_iterations=2000, learning_rate=0.5, print_cost=False):
+    train_set_x_orig, train_y, test_set_x_orig, test_y, classes = load_dataset()
+    assert (train_set_x_orig.shape == (209, 64, 64, 3))
+    assert (train_y.shape == (1, 209))
+    assert (test_set_x_orig.shape == (50, 64, 64, 3))
+    assert (test_y.shape == (1, 50))
+    train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+    test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+    assert (train_set_x_flatten.shape == (12288, 209))
+    assert (test_set_x_flatten.shape == (12288, 50))
+    train_x = train_set_x_flatten/255
+    test_x = test_set_x_flatten/255
+
+    w,b = init_zeros(train_x.shape[0])
+    param, grads, costs = optimize(w, b, train_x, train_y,num_iterations, learning_rate, print_cost)
+    w = param["w"]
+    b = param["b"]
+    Y_prediction_test = predict(w, b, test_x)
+    Y_prediction_train = predict(w, b, train_x)
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - train_y)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - test_y)) * 100))
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test,
+         "Y_prediction_train": Y_prediction_train,
+         "w": w,
+         "b": b,
+         "learning_rate": learning_rate,
+         "num_iterations": num_iterations}
+
+    return d
+
+
+def predict_cat(picname, model):
+    root_path = os.path.split(os.path.realpath(__file__))[0]
+    path = root_path + "/images/" + picname
+    image = np.array(ndimage.imread(path, flatten=False))
+    my_image = scipy.misc.imresize(image, size=(64, 64)).reshape((1, 64 * 64 * 3)).T
+    my_predicted_image = predict(model["w"], model["b"], my_image)
+    return my_predicted_image
--- src/test/python/Neural_Networks_and_Deep_Learning/Logistic_Regression_as_a_Neural_Network_Test.py
+++ src/test/python/Neural_Networks_and_Deep_Learning/Logistic_Regression_as_a_Neural_Network_Test.py
@@ -61,3 +61,56 @@ class Python_Basics_with_Numpy_Test(unittest.TestCase):
         print("b = " + str(params["b"]))
         print("dw = " + str(grads["dw"]))
         print("db = " + str(grads["db"]))
+
+    def test_predict(self):
+        w = np.array([[0.1124579], [0.23106775]])
+        b = 1.5593049248448891
+        X = np.array([[1, 2], [3, 4]])
+        print("predictions = " + str(tc.predict(w, b, X)))
+
+
+    def test_model(self):
+        d = tc.model(num_iterations=2000, learning_rate=0.005,
+                  print_cost=True)
+        index = 5
+        print("y = " + str(self.test_set_y[0, index]) + ", you predicted that it is a \"" + self.classes[
+            int(d["Y_prediction_test"][0, index])].decode("utf-8") + "\" picture.")
+        plt.imshow(self.train_set_x_orig[index, :])
+        plt.show()
+
+
+    def test_iterate_num(self):
+        d = tc.model(num_iterations=2000, learning_rate=0.005,
+                     print_cost=True)
+        costs = np.squeeze(d['costs'])
+        plt.plot(costs)
+        plt.ylabel('cost')
+        plt.xlabel('iterations (per hundreds)')
+        plt.title("Learning rate =" + str(d["learning_rate"]))
+        plt.show()
+
+    def test_learning_rate(self):
+        learning_rates = [0.01, 0.001, 0.0001]
+        models = {}
+        for i in learning_rates:
+            print("learning rate is: " + str(i))
+            models[str(i)] = tc.model(num_iterations=1500,
+                                   learning_rate=i, print_cost=False)
+            print('\n' + "-------------------------------------------------------" + '\n')
+
+        for i in learning_rates:
+            plt.plot(np.squeeze(models[str(i)]["costs"]), label=str(models[str(i)]["learning_rate"]))
+
+        plt.ylabel('cost')
+        plt.xlabel('iterations')
+
+        legend = plt.legend(loc='upper center', shadow=True)
+        frame = legend.get_frame()
+        frame.set_facecolor('0.90')
+        plt.show()
+
+
+    def test_predict_cat(self):
+        model = tc.model()
+        for i in range(6):
+            print(tc.predict_cat(str(i) + ".jpg", model))
